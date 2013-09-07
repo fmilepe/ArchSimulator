1c7f4718b1e9ff299b55a99ac25bfd7a76319e37
fmilepe <fmilepe@gmail.com>
2013-09-07 18:45:22 -0300

    Cpu atualizada com os mux necessários para a arquitetura.

--- src/br/ufrj/dcc/archsimulator/model/Cpu.java
+++ src/br/ufrj/dcc/archsimulator/model/Cpu.java
@@ -1,30 +1,37 @@
 package br.ufrj.dcc.archsimulator.model;
 
+import java.util.BitSet;
+
 
 public class Cpu {
-	public Registrador []registradores;
-	public static final int r0 = 0;
-	public static final int r1 = 1;
-	public static final int ra = 2;
-	public static final int pc = 3;
-	public static final int r2 = 4;
-	public static final int r3 = 5;
-	public static final int rb = 6;
-	public static final int ir = 7;
-	public static final int rend = 8;
-	public static final int rdados = 9;
+	//Índices dos registradores
+	public static final int rdados 	= 0;
+	public static final int r0 		= 1;
+	public static final int r1 		= 2;
+	public static final int ra 		= 3;
+	public static final int pc 		= 4;
+	public static final int r2 		= 5;
+	public static final int r3 		= 6;
+	public static final int rb 		= 7;
+	public static final int ir 		= 8;
+	public static final int rend 		= 9;
 	
+	//Componentes Eletrônicos da CPU
+	public Registrador []registradores;
+	public Mux2E muxMem 	= new Mux2E();
+	public Mux4E muxA 		= new Mux4E();
+	public Mux4E muxB 		= new Mux4E();
 	
 	public Cpu() {
-		registradores[r0] = new RegistradorComSinal();
-		registradores[r1] = new RegistradorComSinal();
-		registradores[ra] = new RegistradorComSinal();
-		registradores[pc] = new RegistradorSemSinal();
-		registradores[r2] = new RegistradorComSinal();
-		registradores[r3] = new RegistradorComSinal();
-		registradores[rb] = new RegistradorComSinal();
-		registradores[ir] = new RegistradorSemSinal();
-		registradores[rend] = new RegistradorSemSinal();
-		registradores[rdados] = new RegistradorComSinal();
+		registradores[r0] 		= new RegistradorComSinal();
+		registradores[r1] 		= new RegistradorComSinal();
+		registradores[ra] 		= new RegistradorComSinal();
+		registradores[pc] 		= new RegistradorSemSinal();
+		registradores[r2] 		= new RegistradorComSinal();
+		registradores[r3] 		= new RegistradorComSinal();
+		registradores[rb] 		= new RegistradorComSinal();
+		registradores[ir] 		= new RegistradorSemSinal();
+		registradores[rend] 	= new RegistradorSemSinal();
+		registradores[rdados] 	= new RegistradorComSinal();
 	}
 }
